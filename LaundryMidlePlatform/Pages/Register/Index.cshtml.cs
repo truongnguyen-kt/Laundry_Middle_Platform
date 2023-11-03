25386fb8229eb77d1369f2e625cbc5b35238ccd6
ducbinh02 <ducbinhnguyen.2002@gmail.com>
2023-11-03 21:40:44 +0700

    Push new code from ducbinh02

--- LaundryMidlePlatform/Pages/Register/Index.cshtml.cs
+++ LaundryMidlePlatform/Pages/Register/Index.cshtml.cs
@@ -1,10 +1,11 @@
-using BusinessObjects.Models;
+﻿using BusinessObjects.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Repository.Implements;
 using Repository.Interface;
 using Repository.IRepository;
+using System.Text.RegularExpressions;
 using Validation;
 
 namespace LaundryMidlePlatform.Pages.Register
@@ -47,12 +48,54 @@ namespace LaundryMidlePlatform.Pages.Register
             if (checkUser.Count > 0)
             {
                 Error = "This email already exists!";
-                OnGet();
+                return OnGet();
 
+            }
+            if (!ValidateInputs())
+            {
+                return OnGet();
             }
             _user.AddNewCustomer(User);
 
             return RedirectToPage("/Index");
+        }
+
+        private bool ValidateInputs()
+        {
+            Utils utils = new Utils();
+            var isValid = true;
+            if (!Regex.IsMatch(User.Phone, "^[0-9]+$"))
+            {
+                ModelState.AddModelError("User.Phone", "Phone must contain only numeric digits (0-9)");
+                isValid = false;
+            }else if (!utils.checkTelephoneFormat(User.Phone))
+            {
+                ModelState.AddModelError("User.Phone", "Phone must has length from 10 to 13!");
+                isValid = false;
+            }
+
+
+            if (User.DateOfBirth == DateTime.MinValue)
+            {
+                ModelState.AddModelError("User.DateOfBirth", "DateOfBirth cannot null");
+                isValid = false;
+            }
+            else
+            {
+                // Tính khoảng thời gian giữa ngày sinh và ngày hiện tại
+                TimeSpan ageDifference = DateTime.Now - (DateTime)User.DateOfBirth;
+
+                // Tính tuổi dựa trên khoảng thời gian
+                int age = (int)(ageDifference.TotalDays / 365.25); // Sử dụng 365.25 để xem xét năm nhuận
+
+                // Kiểm tra xem tuổi có lớn hơn 11 hay không
+                if (age < 12)
+                {
+                    ModelState.AddModelError("User.DateOfBirth", "You must be at least 12 years old.");
+                    isValid = false;
+                }
+            }
+            return isValid;
         }
     }
 }
---
+++ LaundryMidlePlatform/Pages/Shared/_AdminError.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LaundryMidlePlatform.Pages.Shared
+{
+    public class _AdminErrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
