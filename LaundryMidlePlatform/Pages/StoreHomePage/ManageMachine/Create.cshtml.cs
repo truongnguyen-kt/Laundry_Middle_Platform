cc65ab6f255d40220c45aa5800be03ec2597399f
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2023-11-05 01:47:52 +0700

    [Update stash_truong + stash_minh]

---
+++ LaundryMidlePlatform/Pages/StoreHomePage/ManageMachine/Create.cshtml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BusinessObjects.Models;
+using Repository.Implements;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interface;
+
+namespace LaundryMidlePlatform.Pages.StoreHomePage.ManageMachine
+{
+    public class CreateModel : PageModel
+    {
+        //private readonly BusinessObjects.Models.LaundryMiddlePlatformContext _context;
+
+        //public CreateModel(BusinessObjects.Models.LaundryMiddlePlatformContext context)
+        //{
+        //    _context = context;
+        //}
+        private readonly IMachineRepository machineRepository = new MachineRepository();
+        private readonly IUserRepository userRepository = new UserRepository();
+        private readonly IStoreRepository storeRepository = new StoreRepository();
+
+
+        [BindProperty]
+        public WashingMachine WashingMachine { get; set; } = default!;
+        [BindProperty]
+        public Store Store { get; set; } = default;
+        public IActionResult OnGet()
+        {
+            string email = HttpContext.Session.GetString("customerEmail");
+            if (email == null)
+            {
+                return Redirect("../Index");
+            }
+            else
+            {
+                User u = userRepository.findUserByEmail(email);
+                if (u != null)
+                {
+                    if (u.RoleId != 3)
+                    {
+                        return Redirect("../Index");
+                    }
+                    else
+                    {
+                        //List<String> s = new List<String>();
+                        //s.Add(storeRepository.GetStoreById(u.UserId).StoreName);
+                        //ViewData["StoreId"] = new SelectList(s, "StoreName");
+                        //WashingMachine.StoreId = u.UserId;
+                        Store = storeRepository.GetStoreById(u.UserId);
+                        return Page();
+                    }
+                }
+                else
+                {
+                    return Redirect("../Index");
+                }
+            }
+        }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            string email = HttpContext.Session.GetString("customerEmail");
+            if (email == null)
+            {
+                return Redirect("../Index");
+            }
+            else
+            {
+                User u = userRepository.findUserByEmail(email);
+                if (u != null)
+                {
+                    if (u.RoleId != 3)
+                    {
+                        return Redirect("../Index");
+                    }
+                    else
+                    {
+                        WashingMachine.StoreId = Store.StoreId;
+                        WashingMachine.Store = storeRepository.GetStoreById((int)WashingMachine.StoreId);
+                        if (!ModelState.IsValid || WashingMachine == null)
+                        {
+                            return Page();
+                        }
+                        machineRepository.AddWashingMachine(WashingMachine);
+                        
+                        //_context.WashingMachines.Add(WashingMachine);
+                        //await _context.SaveChangesAsync();
+
+                        return RedirectToPage("./Index");
+                    }
+                }
+                else
+                {
+                    return Redirect("../Index");
+                }
+            }
+        }
+    }
+}
---
+++ LaundryMidlePlatform/Pages/StoreHomePage/ManageMachine/Details.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Models;
+using Repository.Implements;
+using Repository.Interface;
+
+namespace LaundryMidlePlatform.Pages.StoreHomePage.ManageMachine
+{
+    public class DetailsModel : PageModel
+    {
+        //private readonly BusinessObjects.Models.LaundryMiddlePlatformContext _context;
+
+        //public DetailsModel(BusinessObjects.Models.LaundryMiddlePlatformContext context)
+        //{
+        //    _context = context;
+        //}
+        private readonly IMachineRepository machineRepository = new MachineRepository();
+        private readonly IUserRepository userRepository = new UserRepository();
+        private readonly IStoreRepository storeRepository = new StoreRepository();
+        [BindProperty]
+        public WashingMachine WashingMachine { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            string email = HttpContext.Session.GetString("customerEmail");
+            if (email == null)
+            {
+                return Redirect("../Index");
+            }
+            else
+            {
+                User u = userRepository.findUserByEmail(email);
+                if (u != null)
+                {
+                    if (u.RoleId != 3)
+                    {
+                        return Redirect("../Index");
+                    }
+                    else
+                    {
+                        if (id == null)
+                        {
+                            return NotFound();
+                        }
+
+                        //var washingmachine = await _context.WashingMachines.FirstOrDefaultAsync(m => m.MachineId == id);
+                        var washingmachine = machineRepository.GetWashingMachineById((int)id);
+                        washingmachine.Store = storeRepository.GetStoreById((int)washingmachine.StoreId);
+                        if (washingmachine == null)
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            WashingMachine = washingmachine;
+                        }
+                        return Page();
+                    }
+                }
+                else
+                {
+                    return Redirect("../Index");
+                }
+            }
+        }
+    }
+}
